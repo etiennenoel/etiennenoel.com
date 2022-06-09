633613e792c026cbb180dcd07dc561bc55abb536
Etienne Noel <etienne.noel@ima-tech.ca>
2022-06-09 14:58:00 -0400

    - Properly setup scully

--- etiennenoel/karma.conf.js
+++ etiennenoel/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/yes'),
+      dir: require('path').join(__dirname, './coverage/etiennenoel'),
       subdir: '.',
       reporters: [
         { type: 'html' },
